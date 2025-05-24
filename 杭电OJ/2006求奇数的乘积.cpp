1538a6ad2e30d315def9ed9ea2003e4c1ceb3294
ctrlcandctrlva <122711277+ctrlcandctrlva@users.noreply.github.com>
2025-05-24 22:22:00 +0800

    Create 2006求奇数的乘积.cpp

---
+++ 杭电OJ/2006求奇数的乘积.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int a[10000];
+int main()
+{
+	int n;
+	while (cin >> n) 
+	{
+		for (int i = 0; i < n; ++i)
+		{
+			int x;
+			cin >> x;
+			a[i] = x;
+		}
+		int prod = 1;
+		for (int i = 0; i < n; ++i)
+		{
+			if (a[i] & 1)		//a[i] % 2 == 0
+				prod *= a[i];
+		}
+		cout << prod << endl;
+	}
+	return 0;
+}
