27b050754083e82a3d6fc4c844d523a7a7ee85bd
ctrlcandctrlva <122711277+ctrlcandctrlva@users.noreply.github.com>
2025-05-24 22:36:49 +0800

    Create 2008数值统计.cpp
    判断浮点数正负

---
+++ 杭电OJ/2008数值统计.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+double a[10000];
+int main()
+{
+	int n;
+	while (cin >> n && n) 
+	{
+		for (int i = 0; i < n; ++i)
+		{
+			double x;
+			cin >> x;
+			a[i] = x;
+		}
+		int pcnt = 0, zcnt = 0, ncnt = 0;
+		for (int i = 0; i < n; ++i)
+		{
+                        //浮点数大于零的判断：大于一个很小的正数
+			if (a[i] > 1e-8)
+				++pcnt;
+                        //小于一个很大的负数
+			else if (a[i] < -1e-8 )
+				++ncnt;
+			else
+				++zcnt;
+		}
+		cout << ncnt << ' ' << zcnt << ' ' << pcnt  << endl;
+	}
+	return 0;
+}
